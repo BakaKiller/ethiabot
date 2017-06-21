6713512bb6810fc6c811cf45c01bd304cade34f4
BakaKiller <totalamor@hotmail.fr>
2017-06-21 03:31:24 +0200

    added a bunch of things

--- ethiareal.js
+++ ethiareal.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const jsonfile = require('jsonfile');
+const fs = require('fs');
+const request = require('request');
 
 let config = require('./settings.js');
 
@@ -8,10 +10,44 @@ let message;
 let messageparts;
 
 let prefix = config.prefix;
+let gifs;
+
+jsonfile.readFile('gifs.json', function (err, obj) {
+    if (err) {
+        console.log(err.message);
+    } else {
+        gifs = obj;
+    }
+});
 
 client.on('ready', function () {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
+function isadmin(userid) {
+    return (config.adminusers[userid] !== undefined);
+}
+
+function sendimage(uri, channel) {
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream('temp')).on('close', function () {
+            fs.readFile('temp', function (err, data) {
+                if (err) {
+                    console.log(err.message);
+                } else {
+                    console.log(channel.type);
+                    channel.sendFile(data).catch(function (error) {
+                        console.log(error);
+                    });
+                    console.log(data);
+                }
+            });
+        });
+    });
+}
 
 client.on('message', msg => {
     if (msg.content.substr(0, prefix.length) === prefix) {
@@ -25,13 +61,23 @@ client.on('message', msg => {
                 msg.reply('Ping !');
                 break;
             case 'setprefix':
-                if (config.adminusers[msg.author.id] !== undefined) {
+                if (isadmin(msg.author.id)) {
                     config.prefix = messageparts[1];
                     prefix = config.prefix;
                     msg.reply('Bien reçu ! Maintenant, pour m\'appeler, utilisez le préfixe "' + prefix + '" !');
                 } else {
                     msg.reply('Déso pas déso, seuls les admins ont un pouvoir sur moi !');
                 }
+                break;
+            case 'hug':
+                let hug;
+                if (messageparts[1] !== undefined && messageparts[1].substr(0, 2) === '<@') {
+                    hug = '<@!' + msg.author.id + '> a envoyé un câlin à ' + messageparts[1] + ' ! ';
+                } else {
+                    hug = 'Câlin !';
+                }
+                msg.channel.send(hug);
+                msg.channel.send(gifs.hug[Math.floor(Math.random() * gifs.hug.length)]);
                 break;
         }
     }
--- settings.js
+++ settings.js
@@ -4,17 +4,48 @@
 const jsonfile = require('jsonfile');
 
 class Settings {
-    constructor () {
-        this.adminusers = {};
-        this.prefix = ';';
+    constructor() {
+        this.Adminusers = {};
+        this.Prefix = ';';
+        let self = this;
         jsonfile.readFile('settings.json', function (err, obj) {
+                if (err) {
+                    console.log(err.message);
+                } else {
+                    self.Adminusers = obj.adminusers;
+                    self.Prefix = obj.prefix;
+                }
+        });
+    }
+
+    save() {
+        let settings = {
+            "prefix": this.Prefix,
+            "adminusers": this.Adminusers
+        };
+        jsonfile.writeFile('settings.json', settings, function (err) {
             if (err) {
                 console.log(err.message);
-            } else {
-                Settings.adminusers = obj.adminusers;
-                Settings.prefix = obj.prefix;
             }
         });
+    }
+
+    set prefix(value) {
+        this.Prefix = value;
+        this.save();
+    }
+
+    get prefix() {
+        return this.Prefix;
+    }
+
+    set adminusers(value) {
+        this.Adminusers = value;
+        this.save();
+    }
+
+    get adminusers() {
+        return this.Adminusers;
     }
 }
 
