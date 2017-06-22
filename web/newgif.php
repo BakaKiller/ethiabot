5b9f67d9d0b38bda8c2173c4ecf471d5bb6dca7a
BakaKiller <totalamor@hotmail.fr>
2017-06-22 22:48:51 +0200

    added a test

--- web/newgif.php
+++ web/newgif.php
@@ -13,27 +13,29 @@ $waitinggifs = json_decode(file_get_contents('../waitinggifs.json'));
 $gifs = json_decode(file_get_contents('../gifs.json'));
 $cats = json_decode(file_get_contents('../cats.json'));
 
-echo 'cat : ' . $cat;
-echo '<br /><br />cats : <pre>' . print_r($cats, true) . '</pre>';
 if (!in_array($cat, $cats)) {
     echo "Toi t'as essayé de tricher èé Heureusement, je suis magnanime. Allez, joue la honnêtement et propose une nouvelle catégorie si celle-là n'existe pas ^^";
     exit;
 }
-
+echo __LINE__;
 $catgifs = $gifs[$cat];
 if (empty($waitinggifs[$cat])) {
     $waitinggifs[$cat] = array();
 }
+echo __LINE__;
 $catwaitinggifs = $waitinggifs[$cat];
 $newgifs = str_replace("\r\n", "\n", $newgifs);
 $newgifs = str_replace("\r", "\n", $newgifs);
+echo __LINE__;
 $newgifs = explode("\n", $newgifs);
+echo __LINE__;
 foreach ($newgifs as $newgif) {
     $newgif = trim($newgif);
     if (!in_array($newgif, $catgifs) && !in_array($newgif, $catwaitinggifs)) {
         $waitinggifs[$cat][] = $newgif;
     }
 }
+echo __LINE__;
 
 if (file_put_contents('../waitinggifs.json', json_encode($waitinggifs))) {
     echo "Votre demande a été enregistrée !";
