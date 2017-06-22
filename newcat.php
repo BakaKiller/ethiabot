ace9d6a454458f1191549cf8dc879866afb1142a
BakaKiller <totalamor@hotmail.fr>
2017-06-22 20:02:05 +0200

    added php form treatment for new categories and gifs propositions

---
+++ newcat.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Baka Killer
+ * Date: 22/06/2017
+ * Time: 19:30
+ */
+
+$newcats = $_POST['newcats'];
+$waitingcats = json_decode(file_get_contents('waitingcats.json'));
+$cats = json_decode(file_get_contents('cats.json'));
+
+$newcats = explode(',', $newcats);
+foreach ($newcats as $newcat) {
+    $newcat = trim($newcat);
+    if (!in_array($newcat, $cats) && !in_array($newcat, $waitingcats)) {
+        $waitingcats[] = $newcat;
+    }
+}
+
+if (file_put_contents('waitingcats.json', json_encode($waitingcats))) {
+    echo "Votre demande a été enregistrée !";
+} else {
+    echo "Il y a eu un problème ! Contactez le gestionnaire du bot pour le lui signaler !";
+}
---
+++ newgif.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Baka Killer
+ * Date: 22/06/2017
+ * Time: 19:30
+ */
+
+$newgifs = $_POST['newgifs'];
+$cat = $_POST['cat'];
+
+$waitinggifs = json_decode(file_get_contents('waitinggifs.json'));
+$gifs = json_decode(file_get_contents('gifs.json'));
+$cats = json_decode(file_get_contents('cats.json'));
+
+if (!in_array($cat, $cats)) {
+    echo "Toi t'as essayé de tricher èé Heureusement, je suis magnanime. Allez, joue la honnêtement et propose une nouvelle catégorie si celle-là n'existe pas ^^";
+    exit;
+}
+
+$catgifs = $gifs[$cat];
+if (empty($waitinggifs[$cat])) {
+    $waitinggifs[$cat] = array();
+}
+$catwaitinggifs = $waitinggifs[$cat];
+$newgifs = str_replace("\r\n", "\n", $newgifs);
+$newgifs = str_replace("\r", "\n", $newgifs);
+$newgifs = explode("\n", $newgifs);
+foreach ($newgifs as $newgif) {
+    $newgif = trim($newgif);
+    if (!in_array($newgif, $catgifs) && !in_array($newgif, $catwaitinggifs)) {
+        $waitinggifs[$cat][] = $newgif;
+    }
+}
+
+if (file_put_contents('waitinggifs.json', json_encode($waitinggifs))) {
+    echo "Votre demande a été enregistrée !";
+} else {
+    echo "Il y a eu un problème ! Contactez le gestionnaire du bot pour le lui signaler !";
+}
