fa691e634eb95660c19713c67398f79d7cf24c2f
BakaKiller <totalamor@hotmail.fr>
2017-07-05 00:30:45 +0200

    updated propositions

--- ethiareal.js
+++ ethiareal.js
@@ -58,6 +58,14 @@ function getgif(type) {
     return gifs[type][Math.floor(Math.random() * gifs[type].length)];
 }
 
+client.on('messageDelete', function(msg) {
+    if (msg.author.id === "130453221331173386") {
+        msg.channel.send('<' + msg.author.id + '> a écrit mais n\'a pas assumé :\n```\n' + msg.content + '\n```')
+    } else if (msg.author.id === "139512885679357953" && msg.content === "test de fou") {
+        msg.channel.send('<' + msg.author.id + '> a réussi un test ! \n```\n' + msg.content + '\n```');
+    }
+});
+
 client.on('message', msg => {
     if (msg.content.substr(0, prefix.length) === prefix) {
         if (msg.author.id === "163688614205718528") {
