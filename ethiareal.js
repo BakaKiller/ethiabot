ecb21e2c9a1aea16a2699cbcea6697d3d3136d62
David Aragon <david.aragon@andil.fr>
2017-07-07 13:52:44 +0200

    added js to validatecat

--- ethiareal.js
+++ ethiareal.js
@@ -25,6 +25,7 @@ const help = "Bonjour ! Voici l'aide de ce bot.\n\nPour exécuter une commande, 
     "`nibble`     exprime son attirance pour le goût de quelqu'un\n" +
     "`facepalm`   exprime sa désapointance sur un sujet quelconque\n" +
     "`clap`       montre une admiration sans égale mais tout à fait placide et platonique\n" +
+    "`blush`      montre une timidité certaine\n" +
     "\nEn cas de questions, n'hésite pas à t'adresser à <@139512885679357953> !\n\n" +
     "Une suggestion ? Envoie ça dans <#326780349793435648>\n" +
     "Une proposition ? Un énième gif ou groupe de gif à ajouter ? Ça se passe à l'adresse suivante :\n" +
@@ -114,6 +115,7 @@ client.on('message', msg => {
             case 'nibble':
             case 'facepalm':
             case 'clap':
+            case 'blush':
                 msg.channel.send(getgif(messageparts[0]));
                 break;
             case 'help':
