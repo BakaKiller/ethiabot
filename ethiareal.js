9a275d7009c1ed37afb3e077aafa357ca0f61644
BakaKiller <totalamor@hotmail.fr>
2017-07-13 00:15:10 +0200

    added an ultimatum

--- ethiareal.js
+++ ethiareal.js
@@ -83,8 +83,10 @@ client.on('messageDelete', function(msg) {
 
 client.on('message', msg => {
     // console.log(msg.channel.id);
-    if (msg.guild.id === '332988697630736394') {
-        msg.guild.members.get('301069123591471114').setNickname(msg.guild.members.get('301069123591471114').nickname.toLowerCase());
+    if (msg.guild) {
+        if (msg.guild.id === '332988697630736394') {
+            msg.guild.members.get('301069123591471114').setNickname(msg.guild.members.get('301069123591471114').nickname.toLowerCase());
+        }
     }
     if (msg.content.substr(0, prefix.length) === prefix) {
         if (msg.author.id === "163688614205718528") {
