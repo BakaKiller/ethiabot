2eeb17b74de43b53da264d4accbe547fa4c915b0
BakaKiller <totalamor@hotmail.fr>
2017-07-20 00:09:57 +0200

    updated gifs

--- ethiareal.js
+++ ethiareal.js
@@ -29,7 +29,8 @@ const help = "Bonjour ! Voici l'aide de ce bot.\n\nPour exécuter une commande, 
     "`pout`       montre un désaccord peu cordial\n" +
     "`smile`      fait preuve de joie o/\n" +
     "`stare`      OwO\n" +
-    "`pantsu`     montre... Enfin, voilà quoi\n" +
+    "`pantsu`     montre... Enfin, voilà quoi" +
+    "`sparkles`   brille !\n" +
     "\nEn cas de questions, n'hésite pas à t'adresser à <@139512885679357953> !\n\n" +
     "Une suggestion ? Envoie ça dans <#326780349793435648>\n" +
     "Une proposition ? Un énième gif ou groupe de gif à ajouter ? Ça se passe à l'adresse suivante :\n" +
@@ -143,6 +144,7 @@ client.on('message', msg => {
             case 'smile':
             case 'stare':
             case 'pantsu':
+            case 'sparkles':
                 msg.channel.send(getgif(messageparts[0]));
                 break;
             case 'help':
