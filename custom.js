ad410295da1e723c262bf5e07bc5c4d934d71cf0
Biquet <Baka-killer@hotmail.fr>
2017-07-29 03:01:18 +0200

    test custom functions

--- custom.js
+++ custom.js
@@ -3,8 +3,6 @@ class CustomFunctions {
         cli.on('messageDelete', function(msg) {
             if (msg.author.id === "130453221331173386") {
                 msg.channel.send('<@' + msg.author.id + '> a écrit mais n\'a pas assumé :\n```\n' + msg.content + '\n```')
-            } else if (msg.author.id === "139512885679357953") {
-                msg.channel.send('T\'inquiètes, mec, ça fonctionne :thumbsup:');
             }
         });
 
